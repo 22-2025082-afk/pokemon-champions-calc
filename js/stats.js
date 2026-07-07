c682c7f59940e63d791ee4afd5c568b644a2bf63
22-2025082-afk <22-2025082@edu.city.izumi.osaka.jp>
2026-07-07 13:27:20 +0900

    Update stats.js

--- js/stats.js
+++ js/stats.js
@@ -1,1 +1,95 @@
+// ========================================
+// Pokémon Champions ダメージ計算機 Ver.1.0
+// stats.js
+// ========================================
 
+import { getNatureMultiplier } from "./natures.js";
+
+/**
+ * HP実数値
+ * Pokémon Champions
+ * HP = 種族値 + 75 + 能力ポイント
+ */
+export function calculateHP(baseHP, hpPoint) {
+    return Number(baseHP) + 75 + Number(hpPoint);
+}
+
+/**
+ * HP以外
+ * floor((種族値 + 20 + 能力ポイント) × 性格補正)
+ */
+export function calculateStat(baseStat, point, natureMultiplier = 1.0) {
+    return Math.floor(
+        (Number(baseStat) + 20 + Number(point)) *
+        Number(natureMultiplier)
+    );
+}
+
+/**
+ * 6能力まとめて計算
+ */
+export function calculatePokemonStats(baseStats, points, natureName) {
+
+    const nature = getNatureMultiplier(natureName);
+
+    return {
+
+        hp: calculateHP(
+            baseStats.hp,
+            points.hp
+        ),
+
+        atk: calculateStat(
+            baseStats.atk,
+            points.atk,
+            nature.atk
+        ),
+
+        def: calculateStat(
+            baseStats.def,
+            points.def,
+            nature.def
+        ),
+
+        spa: calculateStat(
+            baseStats.spa,
+            points.spa,
+            nature.spa
+        ),
+
+        spd: calculateStat(
+            baseStats.spd,
+            points.spd,
+            nature.spd
+        ),
+
+        spe: calculateStat(
+            baseStats.spe,
+            points.spe,
+            nature.spe
+        )
+
+    };
+
+}
+
+/**
+ * 実数値表示用
+ */
+export function statsToText(stats){
+
+    return `
+HP : ${stats.hp}
+
+攻撃 : ${stats.atk}
+
+防御 : ${stats.def}
+
+特攻 : ${stats.spa}
+
+特防 : ${stats.spd}
+
+素早さ : ${stats.spe}
+`;
+
+}
